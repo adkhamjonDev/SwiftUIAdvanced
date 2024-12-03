ccfc29a873e0db3733d5e6a852b3667d8638becc
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-12-03 14:12:55 +0500

    Initial Commit

---
+++ SwiftUIAdvanced/SwiftUIAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedApp.swift
+//  SwiftUIAdvanced
+//
+//  Created by Adkhamjon Rakhimov on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
